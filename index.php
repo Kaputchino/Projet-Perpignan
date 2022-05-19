85a11ae7c958bc051d9db64888a9fcfc6af9ca50
Kaputchino <andreasmulard@gmail.com>
2022-05-19 10:18:50 +0200

    mysqli

--- index.php
+++ index.php
@@ -6,16 +6,8 @@ $servername = "localhost";
 $username = "root";
 $password = " ";
 $db = "projetperpignan";
-try {
-   $dbh = new PDO('mysql:host=localhost;dbname=projetperpignan', $username, $password);
+$dbhost="localhost";
+$conn = new mysqli($dbhost, $username, $password,$db) or die("Connect failed: %s\n". $conn -> error);
 
-   // set the PDO error mode to exception
-   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   echo "Connected successfully";
-   }
-catch(PDOException $e)
-   {
-   echo "Connection failed: " . $e->getMessage();
-   }
 echo "hi";
 ?>
