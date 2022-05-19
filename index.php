a7bcc06a824c884a7e7a3415e8eb6d3a909dab75
Kaputchino <andreasmulard@gmail.com>
2022-05-19 10:07:31 +0200

    update index

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
-require 'php/header.php';
-require 'html/index.html';
+include 'html/index.html';
+
+echo "welcome";
+$servername = "localhost";
+$username = "root";
+$password = " ";
+$db = "projetperpignan";
+try {
+   $dbh = new PDO('mysql:host=localhost;dbname=projetperpignan', $username, $password);
+
+   $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password, $db);
+   // set the PDO error mode to exception
+   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   echo "Connected successfully";
+   }
+catch(PDOException $e)
+   {
+   echo "Connection failed: " . $e->getMessage();
+   }
+echo "hi";
 ?>
