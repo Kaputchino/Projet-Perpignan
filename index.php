ab64482cb68a415f417208782dd49828e0689c16
Kaputchino <andreasmulard@gmail.com>
2022-05-19 11:02:17 +0200

    test

--- index.php
+++ index.php
@@ -8,6 +8,13 @@ $password = "";
 $db = "projetperpignan";
 $dbhost="localhost";
 $conn = new mysqli($dbhost, $username,$password,$db) or die("Connect failed: %s\n". $conn -> error);
-
-echo "hi";
+$userId="0";
+$request="SELECT * FROM `trips` WHERE `idUser`="."0";
+echo $request;
+$stmt=$db->prepare($request);
+$stmt->execute();
+$res=$stmt->get_result()->fetch_assoc();
+foreach($db->query($request) as $row) {
+   print_r($row);
+}
 ?>
