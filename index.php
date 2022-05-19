a57ef35bcc2a6cdeee163c08cff77888bc768778
Kaputchino <andreasmulard@gmail.com>
2022-05-19 11:21:14 +0200

    use of db

--- index.php
+++ index.php
@@ -7,14 +7,18 @@ $username = "root";
 $password = "";
 $db = "projetperpignan";
 $dbhost="localhost";
-$conn = new mysqli($dbhost, $username,$password,$db) or die("Connect failed: %s\n". $conn -> error);
+$connexion = mysqli_connect('localhost', $username, $password);
+mysqli_select_db($connexion, $db); 
+
 $userId="0";
 $request="SELECT * FROM `trips` WHERE `idUser`="."0";
-echo $request;
-$stmt=$db->prepare($request);
-$stmt->execute();
-$res=$stmt->get_result()->fetch_assoc();
-foreach($db->query($request) as $row) {
-   print_r($row);
+$result = mysqli_query($connexion, $request); 
+foreach($result as $value){
+   print_r($value);
+   $requestFirst="SELECT * FROM `stations` WHERE `id`=".$value["idFirstStation"];
+   $resultFirst = mysqli_query($connexion, $requestFirst); 
+   foreach ($resultFirst as $valueFirst){
+      echo("adaptedDrawMarker(".$valueFirst.")");
+   }
 }
 ?>
--- js/map.js
+++ js/map.js
@@ -7,6 +7,15 @@ function drawMarker(stations){
         // On ajoute le marqueur au tableau
     }
 };
+function adaptedDrawMarker(stations){
+    for(station in stations){
+        // On crée le marqueur et on lui attribue une popup
+        var marqueur = L.marker([stations[station].x, stations[station].y]); //.addTo(carte); Inutile lors de l'utilisation des clusters
+        marqueur.bindPopup("<p>"+stations[station].englishName+"</p>");
+        marqueur.addTo(map);
+        // On ajoute le marqueur au tableau
+    }
+};
 function drawPath(path){
     // create a red polyline from an array of LatLng points
 
