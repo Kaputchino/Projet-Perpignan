6f6016fa818021377aa871922350f53283524dc2
Kaputchino <andreasmulard@gmail.com>
2022-05-19 14:53:57 +0200

    test something

--- js/map.js
+++ js/map.js
@@ -22,3 +22,36 @@ function drawPath(path){
 
     var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
 };
+
+function drawMap(){
+    var stations = {
+        "Paris": { "lat": 48.852969, "lon": 2.349903 },
+        "Brest": { "lat": 48.383, "lon": -4.500 },
+        "Quimper": { "lat": 48.000, "lon": -4.100 },
+        "Bayonne": { "lat": 43.500, "lon": -1.467 }
+    };
+    var latlngs = [
+    [45.51, -122.68],
+    [37.77, -122.43],
+    [34.04, -118.2]
+    ];
+var map = L.map('mapRail').setView([0, 0], 1);
+L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        // Il est toujours bien de laisser le lien vers la source des données
+        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+        minZoom: 1,
+        maxZoom: 20
+    }).addTo(map);
+
+
+var openrailwaymap = new L.TileLayer('http://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png',
+    {
+        attribution: '<a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>, Style: <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a> <a href="http://www.openrailwaymap.org/">OpenRailwayMap</a> and OpenStreetMap',
+        minZoom: 1,
+        maxZoom: 20,
+        tileSize: 256
+    }).addTo(map);
+
+    drawMarker(stations);
+    drawPath(latlngs);
+};
