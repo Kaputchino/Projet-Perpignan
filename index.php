f2d62526571295ff8d279b9519063b24ec9fc3dd
Kaputchino <andreasmulard@gmail.com>
2022-05-19 15:20:23 +0200

    Revert "use of db"
    This reverts commit a57ef35bcc2a6cdeee163c08cff77888bc768778.

--- index.php
+++ index.php
@@ -49,18 +49,14 @@ $username = "root";
 $password = "";
 $db = "projetperpignan";
 $dbhost="localhost";
-$connexion = mysqli_connect('localhost', $username, $password);
-mysqli_select_db($connexion, $db); 
-
+$conn = new mysqli($dbhost, $username,$password,$db) or die("Connect failed: %s\n". $conn -> error);
 $userId="0";
 $request="SELECT * FROM `trips` WHERE `idUser`="."0";
-$result = mysqli_query($connexion, $request); 
-foreach($result as $value){
-   print_r($value);
-   $requestFirst="SELECT * FROM `stations` WHERE `id`=".$value["idFirstStation"];
-   $resultFirst = mysqli_query($connexion, $requestFirst); 
-   foreach ($resultFirst as $valueFirst){
-      echo("adaptedDrawMarker(".$valueFirst.")");
-   }
+echo $request;
+$stmt=$db->prepare($request);
+$stmt->execute();
+$res=$stmt->get_result()->fetch_assoc();
+foreach($db->query($request) as $row) {
+   print_r($row);
 }
 ?>
